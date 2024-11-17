1f7f37e7ca5c28f6008f232cf4409aef18e80d0f
Soham Kakkar <kakkarsoham@gmail.com>
2024-11-17 15:59:38 +0530

    modifcations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,28 @@
+// src/App.jsx
 import React, { useState, useEffect } from 'react';
 import LandingAnimation from './components/LandingAnimation';
-import navbar from './components/Navbar';
 import Navbar from './components/Navbar';
 
 function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate loading
     const timer = setTimeout(() => {
       setLoading(false);
-    }, 3000); // Change this duration as needed
+    }, 300); // Change this duration as needed
 
     return () => clearTimeout(timer);
   }, []);
 
   return (
-    <><LandingAnimation loading={loading} />
-    <Navbar /></>
+    <>
+      <LandingAnimation loading={loading} />
+      <Navbar visible={!loading} />
+      <div className='Main-title'>
+        <div className="name">SUGANDHIM</div>
+        <div className="quote">We care for your diet</div>
+      </div>
+    </>
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,20 +1,23 @@
+// src/components/Navbar.jsx
 import React from 'react';
 import './Navbar.css'; // Import the CSS file for styling
 
-const Navbar = () => {
+const Navbar = ({ visible }) => {
     return (
-        <nav className="navbar">
-            <div className="logo">
-                <img src="logo.png" alt="Website Logo" />
+        <nav className={`navbar ${visible ? 'fade-in' : 'fade-out'}`}>
+            <div className="nav">
+                <div className="logo">
+                    Sugandhim
+                </div>
+                <ul className="nav-links">
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="#order">Order</a></li>
+                    <li><a href="#credits">Credits</a></li>
+                </ul>
             </div>
-            <ul className="nav-links">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#menu">Menu</a></li>
-                <li><a href="#order">Order</a></li>
-                <li><a href="#credits">Credits</a></li>
-            </ul>
             <div className="sign-in">
-                <button>Sign In</button>
+                <button>sign in</button>
             </div>
         </nav>
     );
